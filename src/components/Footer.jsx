913d156438d8180d17d5b897fd1accd1e18a97d9
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-11-19 23:04:14 -0500

    Updated remaining image links to follow React syntax

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react"
+import icon_twitter from "/src/assets/icon_twitter.png"
+import icon_facebook from "/src/assets/icon_facebook.png"
+import icon_instagram from "/src/assets/icon_instagram.png"
+import icon_github from "/src/assets/icon_github.png"
+
 
 export default function Footer() {
     return (
         <div className="footer">
-            <img src="/src/assets/icon_twitter.png"></img>
-            <img src="/src/assets/icon_facebook.png"></img>
-            <img src="/src/assets/icon_instagram.png"></img>
-            <img src="/src/assets/icon_github.png"></img>
+            <img src={icon_twitter}></img>
+            <img src={icon_facebook}></img>
+            <img src={icon_instagram}></img>
+            <img src={icon_github}></img>
         </div>
     )
 }
--- src/components/TitleCard.jsx
+++ src/components/TitleCard.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Photo from "/src/assets/me.jpg"
+import icon_email from "/src/assets/icon_email.png"
+import icon_linked from "/src/assets/icon_linked.png"
 
 export default function TitleCard() {
     return (
@@ -9,8 +11,8 @@ export default function TitleCard() {
             <h2>Software Developer</h2>
             <h3><a href="https://www.sdole86.github.io">sdole86.github.io</a></h3>
             <div className="buttons">
-                <button id="email-button"><img src="/src/assets/icon_email.png"></img>Email</button>
-                <button id="linkedin-button"><img src="/src/assets/icon_linked.png"></img>LinkedIn</button>
+                <button id="email-button"><img src={icon_email}></img>Email</button>
+                <button id="linkedin-button"><img src={icon_linked}></img>LinkedIn</button>
             </div>
 
         </div>
